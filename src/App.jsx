ca8835ac0d1061cb9745df4ce9ace0cfa0635244
Iñaki García <igardiet@gmail.com>
2023-03-01 11:26:25 +0100

    imports and code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,72 @@
-import { useState } from 'react'
-import Form from './components/Form'
-import AllTasks from './components/AllTasks'
-
+import { useState } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
+import Form from "./components/Form";
+import EditForm from "./components/EditForm";
+import AllTasks from "./components/AllTasks";
+import ThemeShift from './components/ThemeShift'
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useLocalStorage("To-do-list-wish-list", []);
+  const [editedTask, setEditedTask] = useState(null);
+  const [editing, setEditing] = useState(false);
+  const [preFocus, setPreFocus] = useState(null);
 
   const addTask = (task) => {
-    setTasks(prevState => [...prevState, task])
-  }
-  
+    setTasks((prevState) => [...prevState, task]);
+  };
+
   const editTask = (id) => {
-    setTasks(prevState => prevState.map(t => (t.id == id ? { ...t, checked: !t.checked } : t)))
-  }
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id == id ? { ...t, checked: !t.checked } : t))
+    );
+  };
+
+  const updateTask = (task) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id == task.id ? { ...t, name: task.name } : t))
+    );
+    exitEditMode();
+  };
+
+  const exitEditMode = () => {
+    setEditing(false);
+    preFocus.focus();
+  };
 
   const deleteTask = (id) => {
-    setTasks(prevState => prevState.filter(t => t.id !== id))
-  }
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
+
+  const editMode = (task) => {
+    setEditedTask(task);
+    setEditing(true);
+    setPreFocus(document.activeElement);
+  };
 
   return (
     <div className="container">
-      <header>
+      {/* <header>
         <h1>To do list</h1>
-      </header>
+      </header> */}
+      {editing && (
+        <EditForm
+          editedTask={editedTask}
+          updateTask={updateTask}
+          exitEditMode={exitEditMode}
+        />
+      )}
       <Form addTask={addTask} />
-      {tasks && (<AllTasks tasks={tasks} editTask={editTask} deleteTask={deleteTask} />)}
+      {tasks && (
+        <AllTasks
+          tasks={tasks}
+          editMode={editMode}
+          editTask={editTask}
+          deleteTask={deleteTask}
+        />
+      )}
+      <ThemeShift />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
